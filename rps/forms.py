bbc9e54ab76f813dad66307ca28247e2b9b8fae6
Cade Chen <bc029@bucknell.edu>
2019-10-28 09:47:21 -0700

    update .gitignore

--- rps/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField
-from wtforms.validators import DataRequired
-
-class RPSForm(FlaskForm):
-    rock = SubmitField(label='rock')
-    paper = SubmitField(label='paper')
-    scissor = SubmitField(label='scissor')
